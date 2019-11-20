caef0ec1398bbc6ce676f84e03d4df70c7ec79c6
Jose <jagn1997@hotmail.com>
2019-11-20 18:24:49 -0500

    Adding project

---
+++ MonitorEstados/src/main/java/co/edu/icesi/MonitorEstadosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.icesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorEstadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorEstadosApplication.class, args);
+	}
+
+}
